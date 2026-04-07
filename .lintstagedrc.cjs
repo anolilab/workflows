7bd468f19485469d7b5b4db019b2f61f10c370b3
prisis <d.bannert@anolilab.de>
2026-04-07 22:56:47 +0200

    fix: update deps

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,3 @@
-module.exports = {
-    "**/package.json": ["pnpm exec sort-package-json"],
-    "**/*.{js,cjs,mjs,ts,cts,mts,yml,yaml,json,json5,jsonc,md}": ["pnpm exec prettier --write"],
-    "**/*": ["pnpm exec secretlint"],
-};
+const { defineConfig } = require("@anolilab/lint-staged-config");
+
+module.exports = defineConfig();
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
     extends: ["@anolilab/commitlint-config"],
     rules: {
         // overwrite rules here
